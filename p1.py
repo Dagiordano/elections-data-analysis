1b1c3a89301dc237b9ef676b907368349f9a71a9
Diego Giordano <diegogiordano@Diegos-MacBook-Air.local>
2023-08-29 14:13:37 -0400

    feat: agregar funcion import_data

--- p1.py
+++ p1.py
@@ -1,5 +1,10 @@
 import csv
+import pandas as pd
 
 def import_data():
     file = input('Ingrese nombre del archivo a leer: ')
+    file_csv = pd.read_csv(file, on_bad_lines='skip', sep=';')
+    diccionario = file_csv.to_dict('records')
+    return diccionario 
+
     
